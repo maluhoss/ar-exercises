aa19ee892cabd1b2130abcd8f3e2b1c9fab9daa5
Maliha Hossain <malu.hoss@gmail.com>
2019-04-16 21:48:44 +0000

    added names for all 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+
+richmond = Store.create(name: "Richmond", annual_revenue: 1_260_000, womens_apparel: true, mens_apparel: false)
+
+gastown = Store.create(name: "Gastown", annual_revenue: 190_000, womens_apparel: false, mens_apparel: true)
+
+puts Store.count
