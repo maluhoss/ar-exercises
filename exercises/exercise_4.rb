457cfc66f6fdf7011a64f005ee96e9cb843c4336
Maliha Hossain <malu.hoss@gmail.com>
2019-04-16 22:33:58 +0000

    created instance variables for mens stores and womens stores with filter and printed them

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224_000, womens_apparel: true, mens_apparel: false)
+
+Store.create(name: "Whistler", annual_revenue: 1_900_000, womens_apparel: false, mens_apparel: true)
+
+Store.create(name: "Yaletown", annual_revenue: 430_000, womens_apparel: true, mens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.find_each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(["womens_apparel= ? AND annual_revenue < ?", true, 1_000_000])
+
+@womens_stores.find_each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
