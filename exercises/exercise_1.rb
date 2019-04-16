8907641298adf3b922a22754d3ea51296049c17d
Maliha Hossain <malu.hoss@gmail.com>
2019-04-16 22:02:44 +0000

    created instance variables for 2 stores and updated name for first store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,10 +5,10 @@ puts "----------"
 
 # Your code goes below here ...
 
-burnaby = Store.create(name: "Burnaby", annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Burnaby", annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
 
-richmond = Store.create(name: "Richmond", annual_revenue: 1_260_000, womens_apparel: true, mens_apparel: false)
+Store.create(name: "Richmond", annual_revenue: 1_260_000, womens_apparel: true, mens_apparel: false)
 
-gastown = Store.create(name: "Gastown", annual_revenue: 190_000, womens_apparel: false, mens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190_000, womens_apparel: false, mens_apparel: true)
 
 puts Store.count
