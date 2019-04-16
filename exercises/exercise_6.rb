f781e1303cb7439f0c2568aaa877632ae362c7f3
Maliha Hossain <malu.hoss@gmail.com>
2019-04-16 22:53:02 +0000

    created relationships between models and created employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Maliha", last_name: "Hossain", hourly_rate: 100)
+
+@store1.employees.create(first_name: "Saba", last_name: "Zub", hourly_rate: 50)
+
+@store1.employees.create(first_name: "Amita", last_name: "Parr", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Sarah", last_name: "Farah", hourly_rate: 75)
+
+@store2.employees.create(first_name: "Ziadh", last_name: "Rabbani", hourly_rate: 25)
